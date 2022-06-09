b5cadc70509a32e8b8d3d888979be248601a345f
Matias Wilberger <matias180399@gmail.com>
2022-06-09 19:05:14 -0300

    Agregado de comentario en varias secciones de la implementacion prolog proylcc

--- pengines_server/apps/proylcc/proylcc.pl
+++ pengines_server/apps/proylcc/proylcc.pl
@@ -13,6 +13,12 @@
 %
 % FGrid es el resultado de hacer 'flick' de la grilla Grid con el color Color.
 % Retorna false si Color coincide con el color de la celda superior izquierda de la grilla.
+% flick(+Grid, +Color, +PosX, +PosY, -FGrid).
+% +Grid: el tablero del juego actual.
+% +Color: el color con el que se desea realizar la jugada.
+% +PosX: coordenada de la celda principal.
+% +PosY: segunda coordenada de la celda principal.
+% -FGrid: tablero resultante de haber realizado la jugada.
 
 flick(Grid, Color, PosX, PosY, FGrid):-
     color(Grid,[PosX, PosY],Elem),
@@ -30,7 +36,12 @@ cambiarColorAdyacentes(Grid, [], _Color, Grid).
 %
 % Caso recursivo del predicado de cambio de color,
 % Este predicado se encarga de cambiar el elemento en Grid,
-% en posicion X e Y por el elemento Color
+% en posicion X e Y por el elemento Color.
+% +Grid: igual que en 'flick/5'.
+% [[+PosX, +PosY]| +Resto]: lista de posiciones adyacentes.
+% +Color: igual que en 'flick/5'.
+% -PLista: tablero resultante de cambiar un elemento (en PosX, PosY) por
+% color.
 
 cambiarColorAdyacentes(Grid, [[PosX , PosY] | Resto], Color, PLista) :-
     encontrarLista(Grid,PosX,Lista),
@@ -44,6 +55,7 @@ cambiarColorAdyacentes(Grid, [[PosX , PosY] | Resto], Color, PLista) :-
 % Predicado para encontrar una lista por su posicion (_, Y) en una
 % matriz/grid, con iniciales (0, 0) hasta (n-1, n-1).
 % (Pensado para ser usado con el predicado encontrar elemento).
+%
 
 encontrarLis(Lista, 0, Lista).
 
@@ -87,7 +99,7 @@ compare([[X|Xy]|T],[Z|Zy]):-
 equal(X,X).
 
 %!
-% calcularAdyacentes(+Grid, Start, -Adyacemtes)
+% calcularAdyacentes(+Grid, +Start, -Adyacemtes)
 % Este predicado se encarga de buscar todas las celdas adyacentes a la
 % celda origen
 % Grid: la grilla actual del juego.
@@ -177,19 +189,34 @@ color(Grid, [X,Y], C):-
 ayuda(Grilla, Colores, [PosX,PosY], ColPrincipal, Profundidad, MejorSolucion):-
     length(Grilla, L),
     Total is L*L,
+
+    %Se utiliza este assert para poder saber si una movida en especifico, completa el tablero, y termina el juego
     assert(celdas(Total)),
+
+    %Este findall encuentra todas las posibles soluciones en profundidad a nuestro tablero.
     findall(
         [Solucion, CantCapturadas],
         greedSearch(Grilla, Colores, [PosX,PosY], ColPrincipal, Profundidad, Solucion, CantCapturadas),
         Soluciones),
+
+    %Este findall separa de todas las soluciones, aquellas que terminen el juego.
     findall(
         [SolucionTermina, Total],
         member([SolucionTermina, Total], Soluciones),
         Terminan),
+
+    %Se procede a filtrar de las soluciones, aquella que mas celdas capturan
     filtrarSolucionMayorCapt(Soluciones, MasCapt),
+
+    %Luego se obtiene la menor serie de movimientos que terminan el juego, si existe
     filtrarSolucionTerminanMasCorta(Terminan, MejorTermina),
+
+    % Se prepara para retornar la menor serie de movimientos que terminen el juego. Si esta no existe, se retorna aquella serie de
+    % movimientos que mas celdas capturo en Profundidad.
     encontrarSolucion(MasCapt,MejorTermina, MejorSolucion),
-    retract(celdas(_)).
+
+    %Se retracta el assert realizado al principio del predicado
+    retract(celdas(_)), !.
 
 encontrarSolucion(Sol1,[],Sol1).
 
@@ -244,6 +271,19 @@ encontarMenor([[Jugada, Total]|RestoSoluciones], MejJugAct, Total, [MejJugada,To
 % Este predicado se encarga de realizar una busqueda exhaustiva para
 % lograr encontrar aquellas secuencias de movidas que se pueden realizar
 % en la profundidad, ignorando aquellas jugadas triviales.
+% greedSearch(+Grilla, +Colores, [+PosX, +PosY], +Color, +Profundidad,
+% [-NCol|-Sol], -TotalCapturadas).
+% Grilla: tablero sobre el cual se va a buscar la secuencia de jugadas.
+% Profundidad: maximo de largo de la serie de jugadas a realizar.
+% Colores: igual que en 'ayuda/6'.
+% [PosX, PosY]: posicion principal.
+% Color: color de la ultima jugada o de la posicion principal si es el
+% primer flick realizado (para evitar movimientos sin sentido).
+% -TotalCapturadas: total de celdas capturadas al finalizara la serie de
+% movimientos.
+% [-NCol|-Sol]: NCol es el color con el cual se realizara un flick, Sol
+% es el resto de la secuencia de movidas a encontrar.
+%
 greedSearch(_Grilla, _Colores, _Start, _ColPrincipal, 0, [], 0):-!.
 
 greedSearch(Grilla, Colores, [PosX,PosY], Color, 1, [NCol], TotalCapturadas):-
@@ -271,6 +311,18 @@ greedSearch(Grilla, Colores, [PosX,PosY], Color, Profundidad, [NCol|Sol], TotalC
 % Este predicado se encarga de verificar que una jugada, termine el
 % juego. En caso de que eso suceda, no es necesario seguir buscando para
 % dicha jugada, aun si no se llego a la profundidad dada.
+% controlFinJuego(+Gridmid, +Colores, [+PosX, +PosY], _Color, +NCol,
+% _CantCapt, +ProfMenor, -Sol, -TotalCapturadas).
+% Gridmid: es el tablero resultante de haber realizado flick con el
+% color NCol, a grilla en 'greedSearch/7'.
+% Colores: igual que en 'ayuda/6'.
+% [PosX, PosY]: igual que en 'greedSearch/7'.
+% NCol: Color del ultimo flick que se realizo, a convertirse en Color en
+% el proximo llamado a greedSearch.
+% ProfMenor: la nueva profundidad, 1 menos que Profundidad en
+% 'greedSearch/7'
+% -Sol: el resto de la secuencia de movidas a realizar.
+% -TotalCapturadas: igual que en 'greedSearch/7'.
 controlFinJuego(_, _, _, _,_, Total, _, [], Total):-
     celdas(Total),!.
 
